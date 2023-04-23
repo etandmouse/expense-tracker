bfc7bce138e592b900e6ce4dd6bac97c01c692af
etandmouse <etandmouse@gmail.copm>
2023-04-23 15:30:55 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const routes = require('./routes')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+const PORT = 3000
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is starting on http://localhost:${PORT}`)
+})
