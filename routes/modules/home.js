d4e45cb058b35f516ff7a1928f40880e6e0cfc35
etandmouse <etandmouse@gmail.copm>
2023-04-27 22:06:28 +0800

    feat: modify index.hbs and show the default page data from DB

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
+const Record = require('../../models/records')
+const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
-  res.render('index')
+  let totalAmount = 0
+  Record.find()
+    .lean()
+    .then(records => {
+      Promise.all(records.map(record => {
+        return Category.findById({ _id: record.categoryId })
+          .lean()
+          .then((data) => {
+            record.categoryIcon = data.icon
+            record.date = record.date.toISOString().slice(0, 10).replaceAll("-", "/")
+            totalAmount += record.amount
+            return record
+          })
+      }))
+        .then(() => {
+          res.render('index', { records, totalAmount })
+        })
+    })
+    .catch(err => console.error(err))
 })
 
 module.exports = router
