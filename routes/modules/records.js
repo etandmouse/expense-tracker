4867169335b2202c41829aab3766d845fa64359b
etandmouse <etandmouse@gmail.copm>
2023-04-29 13:22:43 +0800

    feat: Add edit page

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -17,11 +17,33 @@ router.get("/new", (req, res) => {
 
 router.post('/', (req, res) => {
   const { name, date, amount, categoryId } = req.body
-  console.log(req.body)
-  console.log(name, date, amount, categoryId)
   Record.create({ name, date, amount, categoryId })
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
+
+router.get('/:_id/edit', (req, res) => {
+  const _id = req.params._id
+  Record.findOne({ _id })
+    .lean()
+    .then(record => {
+      record.date = record.date.toISOString().slice(0, 10)
+      const categoryId = record.categoryId
+      Category.find()
+        .sort({ _id: 'asc' })
+        .lean()
+        .then(categories => {
+          Category.findById(categoryId)
+            .then(category => {
+              categoryName = category.name
+              res.render('edit', { record, categoryId, category: categories, categoryName })
+            })
+            .catch(error => console.error(error))
+        })
+        .catch(error => console.error(error))
+    })
+    .catch(error => console.error(error))
+})
+
 
 module.exports = router
