47d5961819646bf61cc6d17eaf3a31462ac2b7e8
etandmouse <etandmouse@gmail.copm>
2023-04-29 21:15:59 +0800

    modify recordSeeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -5,6 +5,14 @@ if (process.env.NODE_ENV !== 'production') {
 const db = require('../../config/mongoose')
 const Category = require('../category')
 const Record = require('../records')
+const User = require('../users')
+const bcrypt = require('bcryptjs')
+
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 const SEED_RECORD = [{
   name: '午餐',
@@ -34,20 +42,30 @@ const SEED_RECORD = [{
 }]
 
 db.once('open', () => {
-  Promise.all(Array.from(SEED_RECORD, (item) => {
-    let { name, date, amount, category } = item
-    return Category.findOne({ name: item.category })
-      .lean()
-      .then((item) => {
-        return Record.create({
-          name: name, date: date, amount:amount, categoryId: item._id
-        })
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then((user) => {
+      Promise.all(Array.from(SEED_RECORD, (record) => {
+        let { name, date, amount, category } = record
+        return Category.findOne({ name: category })
+          .lean()
+          .then((category) => {
+            return Record.create({
+              name: name, date: date, amount: amount, categoryId: category._id, userId: user._id
+            })
+              .catch(error => console.error(error))
+          })
           .catch(error => console.error(error))
-      })
-      .catch(error => console.error(error))
-  }))
-    .then(() => {
-      console.log('done')
-      process.exit()
+      }))
+        .then(() => {
+          console.log('done')
+          process.exit()
+        })
     })
 })
